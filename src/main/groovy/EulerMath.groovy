d4df508a6c17583f4b389822c6dce2ee51962db7
Matt Saalfeld <nestharken@gmail.com>
2015-04-01 19:07:43 -0400

    Problem 002

--- src/main/groovy/EulerMath.groovy
+++ src/main/groovy/EulerMath.groovy
@@ -2,4 +2,17 @@ class EulerMath {
 	static def findMultiplesBelow(factor, max) {
 		(0..Math.floor((max-1)/factor)).collect { it * factor }
 	}
+
+	static def fibonacciBelow(max) {
+		def terms = []
+		if (max > 1) {
+			terms = [1, 1]
+			def nextTerm = 2
+			while (nextTerm < max) {
+				terms << nextTerm
+				nextTerm = terms[-1] + terms[-2]
+			}
+		}
+		return terms
+	}
 }
